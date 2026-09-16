0e90ba2ef888fa0a4daf32a9b4ab89b212a2196d
Kieran Wardle <23309814+KieranWardle@users.noreply.github.com>
2026-09-16 12:45:35 +0100

    FMT-160 Added getCaseRequest endpoint

--- src/main/java/uk/gov/ons/census/fwmt/tm/mock/comet/api/managers/CaseManager.java
+++ src/main/java/uk/gov/ons/census/fwmt/tm/mock/comet/api/managers/CaseManager.java
@@ -1,15 +1,18 @@
 package uk.gov.ons.census.fwmt.tm.mock.comet.api.managers;
 
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.ons.census.fwmt.common.data.tm.Case;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import uk.gov.ons.census.fwmt.common.data.tm.CaseRequest;
 
 @Component
 public class CaseManager {
   private Map<String, Case> caseDirectory = new ConcurrentHashMap<>();
+  private Map<String, CaseRequest> caseRequestDirectory = new ConcurrentHashMap<>();
 
   @Value("${customisation.logging.logFlagType.logAllMessages}")
   private boolean logAllMessages;
@@ -28,8 +31,18 @@ public class CaseManager {
     }
   }
 
+  public void addCaseRequest(String caseId, CaseRequest modelCaseRequest) {
+    if (logAllMessages) {
+      caseRequestDirectory.put(caseId, modelCaseRequest);
+    }
+  }
+
   public Case getCase(String id) {
     return caseDirectory.get(id);
+  }
+
+  public CaseRequest getCaseRequest(String id) {
+    return caseRequestDirectory.get(id);
   }
 
   public void reset() {
